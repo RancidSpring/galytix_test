1d44cca3c15cb1f7e9dc0cd2fc6e3f3f75885d99
RancidSpring <levkiselev2000@gmail.com>
2024-09-03 20:37:21 +0200

    Similarity matcher is updated to find and output the closest matches

--- classes/similarity_matcher.py
+++ classes/similarity_matcher.py
@@ -13,13 +13,32 @@ class SimilarityMatcher:
         self.metrics_calculator = metrics_calculator
         self.baseline_embeddings = self.embedder.embed_phrases(baseline_phrases)
 
-    def find_closest_matches(self, phrases: pd.Series):
-        pass
+    def find_closest_matches(self, phrases: pd.Series, mode: str = 'target') -> pd.DataFrame:
+        cleaned_phrases = self.cleaner.apply_cleaning(phrases)
+        embedded_target_phrases = self.embedder.embed_phrases(phrases=cleaned_phrases)
+        distances_from_baseline = self.metrics_calculator.compute_distances_from(
+            embedded_target_phrases, self.baseline_embeddings
+        )
+        best_matches = self.extract_lowest_distance(distances_from_baseline, how=mode)
+        return best_matches
 
     @staticmethod
-    def extract_lowest_distance(distance_df: pd.DataFrame) -> pd.DataFrame:
-        np.fill_diagonal(distance_df.values, np.inf)
-        distance_df = distance_df.where(np.triu(np.ones(distance_df.shape), k=1).astype(bool), np.inf)
+    def extract_lowest_distance(distance_df: pd.DataFrame, how='target') -> pd.DataFrame:
+        """
+        Findes the minimum distance for every index phrase according to the distance value in one of the columns.
+        Supports pairwise and target modes. In target mode it assumes that we are finding the best matches for the
+        passed input phrases, in pairwise mode we assume matching phrases with each other and finding the closest match.
+        :param distance_df: dataframe, where index are phrases to be matched and columns are phrases to match with
+        :param how: mode (see above)
+        :return:
+        """
+        if how == 'pairwise':
+            np.fill_diagonal(distance_df.values, np.inf)
+            distance_df = distance_df.where(np.triu(np.ones(distance_df.shape), k=1).astype(bool), np.inf)
+        elif how == 'target':
+            pass
+        else:
+            raise ValueError('unsupported extract lowest distance mode. Supported: target and pairwise')
         best_matches = distance_df.idxmin(axis=1)
         best_match_df = pd.DataFrame(best_matches, index=distance_df.index, columns=["Best Match"])
         return best_match_df
